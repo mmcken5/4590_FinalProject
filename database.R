6d4c38bd5ed8c126bfdd11d73490eea83f0ef932
Mike McKenzie <Mike McKenzie>
2017-04-09 16:03:12 -0400

    create and use SQLite DB; populate DB with sample data

--- database.R
+++ database.R
@@ -1,5 +1,7 @@
 # Create a database using dplyr with the inputs of your dcf for all the stocks in the DOW
 
+# TODO: Create DB when script is run?
+
 # Install the appropraite packages, if you have not already done so (only need to do once, not each time running program)
 # Uncomment the packages you need to install
 # If you get an error, run RStudio in Admin Mode (right click on the icon and run as administrator)
@@ -8,36 +10,144 @@ install.packages("RMySQL")
 install.packages("data.table")
 devtools::install_github("rstudio/pool")
 devtools::install_github("rstudio/shiny")
+install.packages("tidyquant")
+install.packages("RSQLite")
 
 # Load packages
 library(dplyr)
 library(RMySQL)
+library(tidyquant)
+library(RSQLite)
+
+tq_index("DOWJONES")
+tq_get_options()
+aapl_key_ratios <- tq_get("AAPL", get = "key.ratios")
+aapl_key_ratios[3,2][1]
 
 
-# Sample operations on the data 
-# See first rows of data
-head(airquality)
-# Filter (sub-set) the data
-filter(airquality, Temp > 70)
-# Mutate adds new variables to the data
-mutate(airquality, TempInC = (Temp - 32) * 5 / 9)
-# Summarize aggregates multiple values into a single value
-summarise(airquality, mean(Temp, na.rm = TRUE))
-# The group_by function is used to group data by one or more variables.
-summarise(group_by(airquality, Month), mean(Temp, na.rm = TRUE))
-count(airquality, Month)
+aapl_financials <- tq_get("AAPL", get = "financials")
+aapl_financials
+aapl_financials %>%
+  filter(type == "IS") %>%
+  select(annual) %>%
+  unnest()
+
+
+# # Sample operations on the data 
+# # See first rows of data
+# head(airquality)
+# # Filter (sub-set) the data
+# filter(airquality, Temp > 70)
+# # Mutate adds new variables to the data
+# mutate(airquality, TempInC = (Temp - 32) * 5 / 9)
+# # Summarize aggregates multiple values into a single value
+# summarise(airquality, mean(Temp, na.rm = TRUE))
+# # The group_by function is used to group data by one or more variables.
+# summarise(group_by(airquality, Month), mean(Temp, na.rm = TRUE))
+# count(airquality, Month)
 
 
 # Clear environment
 rm(list = ls())
 
+# Set your working directory
+#setwd("C:/Users/mikem/Projects/4590/4590_FinalProject/")
+
+# Create a SQL Lite DB in the current working directory and connect to it
+my_db <- src_sqlite("stock_db.sqlite3", create = T)
+
+# Put values into DB (each variable is a column)
+id = c(1, 2)
+ticker <- c("TEST1", "TEST2") 
+name <- c("Test Stock 1", "Test Stock 2")
+current_revenue = c(108249, 208249)
+annual_revenue_growth_growth <- c((1+0.06), (1+0.06))
+growth_period <- c(7, 7)
+annual_revenue_growth_stable <- c((1+0.03), (1+0.03))
+stable_period <- c(13, 13)
+annual_cogs_rate <- c(0.8165, 0.8165)
+excess_periods <- c(20, 20)
+current_depreciation <- c(1814, 1814)
+annual_depreciation_growth_now <- c(1-0.0683317211644177,1-0.0683317211644177)
+adg_now_period <- c(1,1)
+annual_depreciation_growth_growth <- c(1.06, 1.06)
+adg_growth_period <- c(5,5)
+annual_depreciation_growth_stable <- c(1.03, 1.03)
+adg_stable_period <- c(14,14)
+tax_rate <- c(0.2422,0.2422)
+current_nol <- c(200000,200000)
+current_capex <- c(7696,7696)
+annual_capex_growth_now <- c(1+0.023939668853227,1+0.023939668853227)
+acg_now_period <- c(1,1)
+annual_capex_growth_growth <- c(1.06, 1.06)
+acg_growth_period <- c(4,4)
+annual_capex_growth_stable <- c(1.03, 1.03)
+acg_stable_period <- c(15,15)
+working_cap_rate <- c(0.05,0.05)
+wcr_period <- c(20,20)
+cap_periods <- c(5,5)
+beta_stock <- c(1,1)
+cost_of_equity <- c(0.07,0.07)
+cost_of_debt <- c(0.025,0.025)
+risk_free_rate <- c(0.02,0.02)
+risk_premium <- c(0.05,0.05)
+publicly_traded_flag <- c(TRUE,TRUE)
+last_traded_price <- c(500,500)
+shares_outstanding <- c(929.3,929.3)
+market_val_debt <- c(0.1,0.1)
+book_val_debt <- c(0.1,0.1)
+book_val_equity <- c(76615,76615)
+debt_capital_ratio <- c(0,0)
+beta_stable_period <- c(0,0)
+debt_capital_ratio_stable_period <- c(0.15,0.15)
+cost_of_debt_stable_period <- c(0.08,0.08)
+growth_rate_stable_period <- c(0.03,0.03)
+operating_expense_stable_period <- c(0.8165,0.8165)
+capex_stable_period <- c(1.1,1.1)
+working_cap_rate_stable_period <- c(0.01,0.01)
+cost_of_debt_flag <- c(TRUE,TRUE)
+
+# Create a data frame using the variables as columns
+stocks = data.frame(id, ticker, name, current_revenue, annual_revenue_growth_growth, growth_period,
+                annual_revenue_growth_stable,stable_period,annual_cogs_rate,excess_periods,
+                current_depreciation,annual_depreciation_growth_now,adg_now_period,
+                annual_depreciation_growth_growth, adg_growth_period,annual_depreciation_growth_stable,
+                adg_stable_period, tax_rate, current_nol, current_capex, annual_capex_growth_now,
+                acg_now_period, annual_capex_growth_growth, acg_growth_period, annual_capex_growth_stable,
+                acg_stable_period, working_cap_rate, wcr_period, cap_periods, beta_stock, cost_of_equity, 
+                cost_of_debt, risk_free_rate, risk_premium, publicly_traded_flag, last_traded_price,
+                shares_outstanding, market_val_debt, book_val_debt, book_val_equity, debt_capital_ratio, 
+                beta_stable_period, debt_capital_ratio_stable_period, cost_of_debt_stable_period,
+                growth_rate_stable_period, operating_expense_stable_period, capex_stable_period, 
+                working_cap_rate_stable_period, cost_of_debt_flag)       
+stocks
+
+# Load the data frame into the database 
+data <- copy_to(my_db, stocks, temporary = FALSE, indexes = list())
+
+
+
+
+
+
+
+# TODO: Download data via tidyquant 
+# TODO: Format that data
+# TODO: Put that data into the DB
+
+
+
+
+
+
+
 # Establish DB connection
-my_db <- src_mysql(
-  dbname = "dowstockinfo",
-  host = "final-project-db-4590.cl74yjtmjrhj.ca-central-1.rds.amazonaws.com",
-  user = "root",
-  password = "nico210wm!#"
-)
+# my_db <- src_mysql(
+#   dbname = "dowstockinfo",
+#   host = "final-project-db-4590.cl74yjtmjrhj.ca-central-1.rds.amazonaws.com",
+#   user = "root",
+#   password = "nico210wm!#"
+# )
 
 # Query DB for 'stocks' table
 my_tbl <- tbl(my_db, "stocks")
@@ -51,20 +161,27 @@ my_tbl
 
 Input_List<- list(
   current_revenue <- my_tbl$current_revenue,
-  annual_revenue_growth <- as.numeric(unlist(strsplit(my_tbl$annual_revenue_growth, split = " "))),
-  annual_cogs_rate <- as.numeric(unlist(strsplit(my_tbl$annual_cogs_rate, split = " "))),
+  # Combine the growth and stable period values
+  annual_revenue_growth <- c(rep(my_tbl$annual_revenue_growth_growth, my_tbl$growth_period),
+                             rep(my_tbl$annual_revenue_growth_stable, my_tbl$stable_period)), 
+  annual_cogs_rate <- c(rep(my_tbl$annual_cogs_rate, my_tbl$excess_periods)),
   excess_periods <- my_tbl$excess_periods,
-  
+
   current_depreciation <- my_tbl$current_depreciation,
-  annual_depreciation_growth <- as.numeric(unlist(strsplit(my_tbl$annual_depreciation_growth, split = " "))),
+  # Combine the rate for now, growth period, and stable period
+  annual_depreciation_growth <- c(rep(my_tbl$annual_depreciation_growth_now, my_tbl$adg_now_period),
+                                  rep(my_tbl$annual_depreciation_growth_growth, my_tbl$adg_growth_period),
+                                  rep(my_tbl$annual_depreciation_growth_stable, my_tbl$adg_stable_period)), 
   
   tax_rate <- my_tbl$tax_rate,
   current_nol <- my_tbl$current_nol,
   
   current_capex <- my_tbl$current_capex,
-  annual_capex_growth <- as.numeric(unlist(strsplit(my_tbl$annual_capex_growth, split = " "))),
+  annual_capex_growth <- c(rep(my_tbl$annual_capex_growth_now, my_tbl$acg_now_period),
+                              rep(my_tbl$annual_capex_growth_growth, my_tbl$acg_growth_period),
+                              rep(my_tbl$annual_capex_growth_stable, my_tbl$acg_stable_period)), 
   
-  working_cap_rate <- as.numeric(unlist(strsplit(my_tbl$working_cap_rate, split = " "))),
+  working_cap_rate <- c(rep(my_tbl$working_cap_rate, my_tbl$wcr_period)),
   
   cap_periods <- my_tbl$cap_periods,
   beta_stock <- my_tbl$beta_stock,
